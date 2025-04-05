1279a3df0d0c105d3e6305f38092fb10d9ea769d
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2025-04-05 14:49:59 +0330

    completed my bookings page

--- src/pages/dashboard/bookings.vue
+++ src/pages/dashboard/bookings.vue
@@ -1,6 +1,26 @@
 <template>
 <div class="tw:grow tw:overflow-y-auto py-6 px-2 tw:flex tw:flex-col">
-	Bookings
+	<div class="tw:w-full tw:max-w-[700px] mx-auto">
+		<v-data-table
+			:headers="headers"
+			:items="bookingStore.bookings as any"
+			density="comfortable"
+			:loading="bookingStore.fetchingBookings"
+			items-per-page="-1"
+			hide-default-footer
+			:mobile="null"
+			mobile-breakpoint="md"
+		>
+			<template v-slot:item.time="{ item }: {item: any}">
+				<template v-if="item.time">
+					<div class="tw:flex tw:items-center tw:justify-end tw:md:justify-center tw:gap-x-1">
+						<span class="mt-1">{{new Date(item.time).toLocaleString()}}</span>
+					</div>
+				</template>
+				<template v-else>as soon as possible</template>
+			</template>
+		</v-data-table>
+	</div>
 </div>
 </template>
 
@@ -15,6 +35,10 @@ import {useBookingStore} from "@/stores/booking";
 const bookingStore = useBookingStore();
 bookingStore.fetchMyBookings();
 
+const headers: any = Object.freeze([
+	{key: 'mentor.name', title: 'Mentor', sortable: true, align: 'start'},
+	{key: 'time', title: 'Time', sortable: true, align: 'end'},
+])
 </script>
 
 <style scoped>
